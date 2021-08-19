bb3356236d28aff1f2645c79cbf00109f2205269
chencyu <chencyu.code@gmail.com>
2021-08-19 11:36:25 +0800

    BDO指令

--- Modules/third_part_app/BDO/BDO.psm1
+++ Modules/third_part_app/BDO/BDO.psm1
@@ -1,8 +1,24 @@
-﻿function 公會招生
+﻿$BDO = "$Env:GDrive/Game/PC Game/黑色沙漠/.shell"
+
+function 公會招生
 {
-    $公會招生文 = Get-Content -Path "$Env:GDrive/Game/PC Game/黑色沙漠/公會招生文.txt"
+    $公會招生文 = Get-Content -Path "$BDO/公會招生文.txt"
     Set-Clipboard -Value $公會招生文
 }
 
+function 會長號
+{
+    Get-Content -Path "$BDO/會長號.txt"
+}
 
-Export-ModuleMember -Function 公會招生
+function 東飾品材料
+{
+    Get-Content -Path "$BDO/東飾品材料.txt"
+}
+
+function 黑洞位置圖
+{
+    &"$BDO/黑洞位置圖.jpg"
+}
+
+Export-ModuleMember -Function 公會招生,會長號,東飾品材料,黑洞位置圖
