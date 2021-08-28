6c3e6339af55d9ac67be8905b23268d53af4ca48
chencyu <chencyu.code@gmail.com>
2021-08-28 11:16:08 +0800

    [Update] 因應環境變數調整，改寫prompt

--- pwsh_profiles/appearance.ps1
+++ pwsh_profiles/appearance.ps1
@@ -2,7 +2,7 @@
 {
     Write-Host "$(Get-Date -format "(yyyy.MMMM.dd)(tt hh:mm)")" -ForegroundColor Blue -NoNewline
     Write-Host "[PowerShell]" -ForegroundColor DarkYellow
-    Write-Host "$Env:USER@$Env:HOSTNAME" -ForegroundColor Green -NoNewline
+    Write-Host "$USER@$HOSTNAME" -ForegroundColor Green -NoNewline
     Write-Host ":[" -NoNewline
     Write-Host "$(Get-Location)" -ForegroundColor Blue -NoNewline
     Write-Host "]" -NoNewline
