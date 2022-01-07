49f2f2c75c099196282af9f9d51df835ff665aa7
chencyu <chencyu.personal@gmail.com>
2022-01-08 00:43:21 +0800

    Print various debugging information.

--- Modules/third_part_app/youtube-dl/youtube-dl.psm1
+++ Modules/third_part_app/youtube-dl/youtube-dl.psm1
@@ -1,6 +1,6 @@
 ﻿function yt($URL)
 {
-    youtube-dl -f bestaudio -x --audio-format mp3 --audio-quality 0 "$URL"
+    youtube-dl -f bestaudio -v -x --audio-format mp3 --audio-quality 0 "$URL"
 }
 
 Export-ModuleMember -Function yt
