ff69f4e3ea2bfbc3268b489635bc226e3f45de1d
chencyu <chencyu.personal@gmail.com>
2022-02-09 18:59:05 +0800

    [Feature] Batch replace filename

--- Modules/file_manager/file_manager.psm1
+++ Modules/file_manager/file_manager.psm1
@@ -13,3 +13,25 @@
 
     robocopy  "$Source" "$Destination" /MT:${Env:NUMBER_OF_PROCESSORS} /XO /UNICODE | Out-Null
 }
+
+function Replace-FileName
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory, Position = 0)]
+        [string]
+        $Obsolete,
+        [Parameter(Position = 1)]
+        [string]
+        $Replacement = ""
+    )
+
+    $(Get-ChildItem).Name | ForEach-Object -Process {
+        if ($_ -match "$Obsolete")
+        {
+            $NewName = $_.Replace("$Obsolete", "$Replacement")
+            Move-Item -Path $_ -Destination $NewName
+        }
+    }
+}
